a269fd3cacf2a0b5b3d2930abd2cf33769d9a3de
CjiW <cao_jingwei@qq.com>
2023-10-27 14:43:13 +0800

    lab1-3: irq

--- kernel/strap.c
+++ kernel/strap.c
@@ -37,8 +37,8 @@ void handle_mtimer_trap() {
   // TODO (lab1_3): increase g_ticks to record this "tick", and then clear the "SIP"
   // field in sip register.
   // hint: use write_csr to disable the SIP_SSIP bit in sip.
-  panic( "lab1_3: increase g_ticks by one, and clear SIP field in sip register.\n" );
-
+  g_ticks++;
+  write_csr(sip, !SIP_SSIP);
 }
 
 //
